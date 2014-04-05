aab98954e53b19735dfe225efadbb307aab1ed9c
Hazardius <hazardiusam@gmail.com>
2014-04-05 15:24:35 +0200

    Unicode escapes - done.

---
+++ cd_to_solrd/files.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+""" This file contains logic of file IN/OUT operations. """
+
+def open_file(path):
+    """ Opening a file specified by a path. Returns a file content."""
+    f = open(path, 'r')
+    insides = u""
+    for line in f:
+    	insides += line
+    return insides.decode('unicode_escape')
---
+++ cd_to_solrd/files_test.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from files import open_file
+
+class pta_files_tests(unittest.TestCase):
+
+    def test_opening_of_correct_txt(self):
+        self.maxDiff = None
+        results = open_file("sample_text_to_test.txt")
+        expected = u"test\nof\nfiles\nIs it proper?\nKraków\nPOKÓJ\npiętrze w garażu"
+        self.assertEqual(expected, results)
+
+if __name__ == '__main__':
+    unittest.main()
+
