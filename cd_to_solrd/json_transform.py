0eed08f371b64f82ea8b641b7c85722255b420c1
Hazardius <hazardiusam@gmail.com>
2014-04-10 14:26:14 +0200

    Correction to make best fit for solr json.

--- cd_to_solrd/json_transform.py
+++ cd_to_solrd/json_transform.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import json
+import md5
 import re
 import string
 import sys
+import uuid
 
 """ This file contains Json transformation methods used by a Flat Searcher."""
 
@@ -22,13 +24,23 @@ def edit_as_dict(insides):
     json_insides = json.loads(insides)
     for ad in json_insides:
         if u"url" in ad:
-            ad[u"id"] = ad[u"url"]
+            # TODO: Convert to UUID
+            m = md5.new(ad[u"url"])
+            ad[u"id"] = str(uuid.UUID(m.hexdigest()))
         if u"area" in ad:
             ad[u"area"] = re.sub("[^0123456789\.,]","",ad[u"area"])
         if u"price" in ad:
             ad[u"price"] = re.sub("[^0123456789\.,]","",ad[u"price"])
         if u"rooms" in ad:
             ad[u"rooms"] = re.sub("[^0123456789\.,]","",ad[u"rooms"])
+        if u"date" in ad:
+            data = ad[u"date"].split(u".")
+            ad[u"_version_"] = long(data[2] + data[1] + data[0])
+        else:
+            ad[u"_version_"] = 0
+        if u"desc" in ad:
+            ad[u"text"] = ad[u"desc"]
+            del ad[u"desc"]
         # ad[u"core"] = u"collection1"
         # If we can delete this entry - uncomment.
         if len(ad[u"rooms"]) == 0:
