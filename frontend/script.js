4b115d872e01e0912df0039f1d21ffe973bacb91
Oliwer <oliwer.iwanicki@gmail.com>
2014-04-17 00:28:18 +0200

    front-end update

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,36 @@
 function onSearch() {
 	
-	var query = $('#query').val();
-	if (query.length == 0) {
-		return;
-	}
-
+	var query = $('#query').val(),
+		location = $('#query').val(),
+		minArea = $('#area').val(),
+		maxPrice = $('#price').val(),
+		roomsNo = $('#rooms').val()
+		url = 'http://94.72.127.27:8983/solr/collection1/select?q=text%3A*';
+	
 	query.replace(/\s+/g,'+');
 	
+	if (query.length > 0) {
+		url += query + '*';
+	}
+
+	if (location.length > 0) {
+		url += '%0Aaddress%3A*' + location + '*%0A';
+	}	
+	
+	url += '&wt=json&indent=true';
+
 	$.ajax({
 		type: 'GET',
-		url: 'http://109.173.222.224:8983/solr/collection1/select?q=text%3A*' + query + '*&wt=json&indent=true',
+		url: url,
 		dataType: 'json',
 		success: function(data) {
-			appendResults(data.response.docs)
+			appendResults(data.response.docs, minArea, maxPrice, roomsNo)
 		},
 		error: function(XMLHttpRequest, textStatus, errorThrown) {
 			alert("error: " + textStatus);    
 		}
 	})
+	
 }
 
 function onReady() {
@@ -30,19 +43,45 @@ function onReady() {
 	});
 }
 
-function appendResults(docs) {
+function appendResults(docs, minArea, maxPrice, roomsNo) {
+	
 	console.log(docs);
 	$('#results').empty();
+	var total = docs.length;
 
-	var total = 'Found ' + docs.length + ' results';
-	$('#results').append('<div>' + total + '</div><br/>');
-	
 	$.each(docs, function(i, item) {
-		$('#results').append($('<div>' + item.title + '</div>'));
-		$('#results').append($('<div>' + item.text + '</div><br/>'));
+		
+		maxPrice = parseInt(maxPrice, 10);
+		if (maxPrice !== '' && parseInt(item.price, 10) > maxPrice) {
+			total--;
+			return;
+		}
+	
+		minArea = parseInt(minArea, 10);
+		if (minArea !== '' && parseInt(item.area, 10) < minArea) {
+			total--;
+			return;
+		}
+				
+		if (roomsNo !== 'dowolna') {
+			if (roomsNo !== item.rooms) {
+				total--;
+				return;
+			}
+		}
+				
+		$('#results').append($(
+		'<div class="result"><p class="resultTitle">' 
+		+ item.title + '</p><p class="resultTitle">' 
+		+ item.area + ' m | ' 
+		+ item.price + ' zł | ' 
+		+ item.rooms + ' pokoje | <a href="' + item.url +' " class="resultLink">link</a></p>'
+		+ ' <p class="resultText">' 
+		+ item.text + '</p></div>'));
+
 	});
-
-
+	
+	$('#results').prepend('<p id="resultInfo">Found ' + total + ' results</p><br/>');
 }
 
 $(document).ready(onReady);
